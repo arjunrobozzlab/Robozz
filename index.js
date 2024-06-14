f45d12e32a9480a25034bb601fb23eca359b393e
arjunrobozzlab <arjun.robozz@gmail.com>
2024-06-14 16:51:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/robozz', (req, res) => {
+    res.send('Robozz Lab!')
+  })
+  app.get('/login', (req, res) => {
+    res.send('<h1>Please Login at Robozz Lab!</h1>')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
